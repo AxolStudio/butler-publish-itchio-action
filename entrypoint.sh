eedd69138f348b66dda13a4001ace2c45cb6fe73
Tim I Hely <seifertim@gmail.com>
2021-02-04 11:43:33 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -e
 
+dnf install unzip -y
+
+"curl -L -o butler.zip https://broth.itch.ovh/butler/${BUTLER_VER}/LATEST/archive/default" \
+    && unzip butler.zip \
+    && cp butler /usr/bin \
+    && chmod +x /usr/bin/butler
+
 export BUTLER_API_KEY=$BUTLER_CREDENTIALS
 
 versionArgument=""
@@ -13,5 +20,5 @@ then
     versionArgument="--userversion-file ${VERSION_FILE}"
 fi
 
-echo "butler push \"$PACKAGE\" $ITCH_USER/$ITCH_GAME:$CHANNEL ${versionArgument}"
-butler push "$PACKAGE" $ITCH_USER/$ITCH_GAME:$CHANNEL ${versionArgument}
+echo "/usr/bin/butler push \"$PACKAGE\" $ITCH_USER/$ITCH_GAME:$CHANNEL ${versionArgument}"
+/usr/bin/butler push "$PACKAGE" $ITCH_USER/$ITCH_GAME:$CHANNEL ${versionArgument}
